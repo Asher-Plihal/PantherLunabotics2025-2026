f0209bb199f9448dc7b864cf5d17a9423e096e8f
Asher Plihal <144475675+roialrogue@users.noreply.github.com>
2026-03-10 19:45:04 -0400

    added jetson ip

--- mission_control/control.py
+++ mission_control/control.py
@@ -160,5 +160,5 @@ class Control:
         self.client.stop()
 
 if __name__ == "__main__":
-    server_ip = "100.76.221.110"  # RP IP = "100.76.221.110"
+    server_ip = "100.87.109.70"  # RP IP = "100.76.221.110" Jetson IP = "100.87.109.70"
     Control(server_ip).run()
