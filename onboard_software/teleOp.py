a71df0f6f444ed82f910c2df5680cce55e1e8700
Asher Plihal <144475675+roialrogue@users.noreply.github.com>
2026-03-12 16:53:16 -0400

    fixed teleOp

--- onboard_software/teleOp.py
+++ onboard_software/teleOp.py
@@ -11,39 +11,28 @@ class TeleOp:
     def __init__(self, robot: robot.Robot):
         self.robot = robot
         self._last_update_time = time.monotonic()
+        self._button_drive_active = False
 
     # Called only when there is a button event
     def on_button_event(self, button, is_pressed):
-        if button == 'A':
-            if is_pressed:
-                #Forward
-                if robot_params.RobotConfig.useDrivetrain:
-                    self.robot.drivetrain.set_power(0.5,0.5,0.5,0.5)
-        elif button == 'Y':
-            if is_pressed:
-                #Backward
-                if robot_params.RobotConfig.useDrivetrain:
-                    self.robot.drivetrain.set_power(-0.5,-0.5,-0.5,-0.5)
-        elif button == 'B':
-            if is_pressed:
-                #Strafe right
-                if robot_params.RobotConfig.useDrivetrain:
-                    self.robot.drivetrain.set_power(-0.5,-0.5,0.5,0.5)
-        elif button == 'X':
-            if is_pressed:
-                #Strafe left
-                if robot_params.RobotConfig.useDrivetrain:
-                    self.robot.drivetrain.set_power(0.5,0.5,-0.5,-0.5)
-        elif button == 'LB':
-            if is_pressed:
-                #Turn left
-                if robot_params.RobotConfig.useDrivetrain:
-                    self.robot.drivetrain.set_power(-0.5,0.5,-0.5,0.5)
-        elif button == 'RB':
-            if is_pressed:
-                #Turn right
-                if robot_params.RobotConfig.useDrivetrain:
-                    self.robot.drivetrain.set_power(0.5,-0.5,0.5,-0.5)
+        if button in ('A', 'Y', 'B', 'X', 'LB', 'RB'):
+            if robot_params.RobotConfig.useDrivetrain:
+                self._button_drive_active = is_pressed
+                if is_pressed:
+                    if button == 'A':
+                        self.robot.drivetrain.set_power(0.5,0.5,0.5,0.5)
+                    elif button == 'Y':
+                        self.robot.drivetrain.set_power(-0.5,-0.5,-0.5,-0.5)
+                    elif button == 'B':
+                        self.robot.drivetrain.set_power(-0.5,-0.5,0.5,0.5)
+                    elif button == 'X':
+                        self.robot.drivetrain.set_power(0.5,0.5,-0.5,-0.5)
+                    elif button == 'LB':
+                        self.robot.drivetrain.set_power(-0.5,0.5,-0.5,0.5)
+                    elif button == 'RB':
+                        self.robot.drivetrain.set_power(0.5,-0.5,0.5,-0.5)
+                else:
+                    self.robot.drivetrain.set_power(0.0,0.0,0.0,0.0)
         elif button == 'DPAD_UP':
             if is_pressed:
                 #Intake Auger
@@ -66,7 +55,7 @@ class TeleOp:
 
     """Called at 50Hz — put all periodic tasks here."""
     def periodic_loop(self):
-        if robot_params.RobotConfig.useDrivetrain:
+        if robot_params.RobotConfig.useDrivetrain and not self._button_drive_active:
             self.robot.drivetrain.drive_task(self.robot.controller.AxisValues.y, self.robot.controller.AxisValues.x, self.robot.controller.AxisValues.yaw_rate)
 
         # Check for joystick drift
