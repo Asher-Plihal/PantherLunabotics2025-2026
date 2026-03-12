21e8c03c285cabc7cbdfacaaeeb437897975fb3e
Asher Plihal <144475675+roialrogue@users.noreply.github.com>
2026-03-12 17:02:51 -0400

    clean up teleOp with new remapings

--- onboard_software/subsystems/drivetrain.py
+++ onboard_software/subsystems/drivetrain.py
@@ -86,9 +86,9 @@ class Drivetrain:
         self.mc.set_motor_duty_cycle(self.left_motor_ids[1], Util.clip(back_left_power, -self.max_speed, self.max_speed))
 
     def drive_task(self, y_axis, x_axis, turning_axis):
-        y_axis = Util.apply_deadzone(y_axis, 0.05)
-        x_axis = Util.apply_deadzone(x_axis, 0.05)
-        turning_axis = Util.apply_deadzone(turning_axis, 0.05)
+        y_axis = Util.apply_deadzone(y_axis, 0.08)
+        x_axis = Util.apply_deadzone(x_axis, 0.08)
+        turning_axis = Util.apply_deadzone(turning_axis, 0.08)
 
         y = (math.atan(5 * y_axis) / math.atan(5))
         x = (math.atan(5 * x_axis) / math.atan(5)) * 1.1 # Strafing compensation
--- onboard_software/teleOp.py
+++ onboard_software/teleOp.py
@@ -14,43 +14,47 @@ class TeleOp:
         self._button_drive_active = False
 
     # Called only when there is a button event
+    # Drivetrain: DPAD_UP = forward, DPAD_DOWN = backward, DPAD_LEFT = strafe left, DPAD_RIGHT = strafe right
+    # Drivetrain: LB = turn left, RB = turn right
+    # Auger: Y = intake, A = outtake, B = stop auger
     def on_button_event(self, button, is_pressed):
-        if button in ('A', 'Y', 'B', 'X', 'LB', 'RB'):
+        if button in ('DPAD_UP', 'DPAD_DOWN', 'DPAD_LEFT', 'DPAD_RIGHT', 'LB', 'RB'):
             if robot_params.RobotConfig.useDrivetrain:
                 self._button_drive_active = is_pressed
                 if is_pressed:
-                    if button == 'A':
+                    if button == 'DPAD_UP':
+                        # Drive forward
                         self.robot.drivetrain.set_power(0.5,0.5,0.5,0.5)
-                    elif button == 'Y':
+                    elif button == 'DPAD_DOWN':
+                        # Drive backward
                         self.robot.drivetrain.set_power(-0.5,-0.5,-0.5,-0.5)
-                    elif button == 'B':
-                        self.robot.drivetrain.set_power(-0.5,-0.5,0.5,0.5)
-                    elif button == 'X':
+                    elif button == 'DPAD_LEFT':
+                        # Strafe left
                         self.robot.drivetrain.set_power(0.5,0.5,-0.5,-0.5)
+                    elif button == 'DPAD_RIGHT':
+                        # Strafe right
+                        self.robot.drivetrain.set_power(-0.5,-0.5,0.5,0.5)
                     elif button == 'LB':
+                        # Turn left
                         self.robot.drivetrain.set_power(-0.5,0.5,-0.5,0.5)
                     elif button == 'RB':
+                        # Turn right
                         self.robot.drivetrain.set_power(0.5,-0.5,0.5,-0.5)
                 else:
                     self.robot.drivetrain.set_power(0.0,0.0,0.0,0.0)
-        elif button == 'DPAD_UP':
+        elif button == 'Y':
             if is_pressed:
-                #Intake Auger
+                # Auger intake
                 print("Intake Auger")
                 self.robot.auger.intake()
-        elif button == 'DPAD_DOWN':
+        elif button == 'A':
             if is_pressed:
-                #Outtake Auger
+                # Auger outtake
                 print("Outtake Auger")
                 self.robot.auger.outtake()
-        elif button == 'DPAD_LEFT':
+        elif button == 'B':
             if is_pressed:
-                #Off Drivetrain
-                if robot_params.RobotConfig.useDrivetrain:
-                    self.robot.drivetrain.set_power(0.0,0.0,0.0,0.0)
-        elif button == 'DPAD_RIGHT':
-            if is_pressed:
-                #Off Auger
+                # Stop auger
                 self.robot.auger.stop()
 
     """Called at 50Hz — put all periodic tasks here."""
